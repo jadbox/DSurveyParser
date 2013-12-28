6d28bdda7b17840c09c4eacfd9157a14e24467cf
Jonathan Dunlap <jadit2@gmail.com>
2013-12-28 10:21:08 -0800

    fixed timer

--- main.d
+++ main.d
@@ -6,6 +6,7 @@ import std.csv;
 import std.file;
 import std.algorithm;
 import std.parallelism;
+import std.datetime;
 import std.concurrency, core.thread;
 
 struct Layout
@@ -33,7 +34,7 @@ void getTopFeatureRequests() {
 	receive(
 		(bool end) { stopped = true; },
 		(Layout record) {
-			if(startsWith(record.d, "I a") || startsWith(record.c, "Every day")) {++sat;} // ingore satisfied prefs
+			if(startsWith(record.d, "I a") || startsWith(record.c, "Every d")) ++sat;
 			else {
 				++unsat;
 				//if(!startsWith(record.c, "Have not")) continue; // have NOT used brackets
@@ -72,10 +73,13 @@ class Timer
 
 void main(string[] args)
 {
+	//scope bench = new Timer();
+
 	writeln("Opening CSV...");
+	StopWatch sw;
+	sw.start();
 	auto data = File("tabdata.csv", "r");
 	writeln(  "Loading done." );
-	scope bench = new Timer();
 
 	auto records = csvReader!Layout(data.byLine().joiner("\n"));
 	auto tid = spawn(&getTopFeatureRequests);
@@ -87,14 +91,14 @@ void main(string[] args)
 	receiveOnly!bool(); //wait untill finish flag
 
 	writefln("=== Usat: %d, sat: %d", unsat, sat);
-	//auto keys = counts.keys;
 	auto t(string a, string b) {
 		return counts[a] > counts[b];
 	}
 	string[] keys = counts.keys;
 	sort!t(keys);
+	sw.stop();
 	int ftotal=0;
 	foreach(ref k; keys) if(counts[k] > 2) writefln("%s -- %s", k, to!string(counts[k]));
-	writeln( "file closed" );
+	writeln( "file closed, microseconds time:", sw.peek().usecs );
 }
 
